553dabf7f84ff902fc168512cba21dd9b3be8001
deerain <d33.i2.1n@gmail.com>
2023-06-20 18:08:09 +0300

    [fix] мелкие правки

--- replays/views.py
+++ replays/views.py
@@ -1,25 +1,27 @@
 from django.forms.models import BaseModelForm
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, redirect
-from django.views.generic import CreateView
-from django.urls import reverse
+from django.shortcuts import get_object_or_404
+from django.views import generic
 
 from tasks.models import Task
 
 from replays.forms import ReplayForm
+from replays.models import Replay
 from replays import services as replay_services
 
 
-class ReplayCreateView(CreateView):
-    model = Task
+class ReplayCreateView(generic.edit.ModelFormMixin, generic.View):
+    model = Replay
     form_class = ReplayForm
+
+    def get_success_url(self) -> str:
+        return self.request.headers.get('referer')
 
     def form_valid(self, form: BaseModelForm) -> HttpResponse:
         task_id = self.request.GET.get('task_id')
-        task = get_object_or_404(self.model, pk=task_id)
+        task = get_object_or_404(Task, pk=task_id)
 
         replay_services.create_replay(task=task, user=self.request.user,
                                       **form.changed_data)
 
-        return redirect(reverse('tasks:detail', kwargs={
-            'category_slug': task.category.slug, 'slug': task.slug}))
+        return super().form_valid(form)
