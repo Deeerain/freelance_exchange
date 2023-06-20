db1b855455c58189ebd31ce382286adbd6b62dc2
deerain <d33.i2.1n@gmail.com>
2023-06-20 17:54:19 +0300

    Разбил логику

--- tasks/services.py
+++ tasks/services.py
@@ -1,16 +1,24 @@
-from django.db.models import Count
+from django.db import models
 
-from tasks import models
+from tasks import models as task_models
 
 
 def task_create(employer, **kwargs):
     models.Task.objects.create(employer, **kwargs)
 
 
-def task_all(**kwargs):
-    return models.Task.objects.select_related('category')\
-        .annotate(replay_count=Count('replays'))\
+def task_all(**kwargs) -> models.QuerySet[task_models.Task]:
+    return task_models.Task.objects.filter(**kwargs)
+
+
+def task_all_with_info(**kwargs) -> models.QuerySet[task_models.Task]:
+    return task_models.Task.objects.select_related('category')\
+        .annotate(replay_count=models.Count('replays'))\
         .filter(**kwargs)
+
+
+def task_all_with_replays(**kwargs):
+    return task_all().prefetch_related('replays').filter(**kwargs)
 
 
 def task_get_all_by_employer(employer):
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,9 @@
-from typing import Any
+from typing import Any, Dict
 from django.db.models.query import QuerySet
 from django.forms.models import BaseModelForm
 from django.http import HttpResponse
 from django.shortcuts import redirect, get_object_or_404, render
-from django.views.generic import ListView, DetailView, CreateView, View
+from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 
@@ -16,7 +16,7 @@ from replays.models import Replay
 from tasks import services as task_services
 
 
-class TaskCreateView(LoginRequiredMixin, CreateView):
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
     permission_required = 'tasks.add_task'
     login_url = 'accounts:signin'
     model = Task
@@ -28,7 +28,7 @@ class TaskCreateView(LoginRequiredMixin, CreateView):
         return redirect(reverse('tasks:list'))
 
 
-class TaksListView(ListView, SearchMixin):
+class TaksListView(generic.ListView, SearchMixin):
     model = Task
     template_name = 'task/list.html'
     search_fields = ['title', 'description']
@@ -44,17 +44,23 @@ class TaksListView(ListView, SearchMixin):
         return task_services.task_all(**filter_values)
 
 
-class TaskDetailView(DetailView):
+class TaskDetailView(generic.edit.FormMixin, generic.DetailView):
     model = Task
     form_class = ReplayForm
     context_object_name = 'task'
     template_name = 'task/detail.html'
 
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context.setdefault('form', self.get_form())
+
+        return context
+
     def get_queryset(self) -> QuerySet[Any]:
-        return task_services.task_all()
+        return task_services.task_all_with_replays()
 
 
-class TaskReplayView(View):
+class TaskReplayView(generic.View):
     def get(self, request, category_slug, slug):
         task = get_object_or_404(Task, slug=slug)
         can_replay = not Replay.objects.filter(
